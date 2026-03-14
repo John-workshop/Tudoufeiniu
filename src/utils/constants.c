f4f2ce4d3e58761c606984058ee3710b36466f5c
John Alexander <johnalexander161107@gmail.com>
2026-03-15 00:51:02 +0800

    Add files via upload

--- src/utils/constants.c
+++ src/utils/constants.c
@@ -1,3 +1,6 @@
+// Code made by John (君亚) at 2026/3/7 23:37 PM
+// Code made by Valerie (陈飞翔) at 2026/3/7 23:43 PM
+
 #ifndef CONSTANTS_H
 #define CONSTANTS_H
 
--- src/utils/constants.h
+++ src/utils/constants.h
@@ -1,3 +1,7 @@
+// Code made by John (君亚) at 2026/3/7 23:58 PM
+// Code made by Valerie (陈飞翔) at 2026/3/7 00:12 PM
+
+// Note: Ensure src/utils/constants.c is deleted as it is a duplicate of this header.
 #ifndef CONSTANTS_H
 #define CONSTANTS_H
 
